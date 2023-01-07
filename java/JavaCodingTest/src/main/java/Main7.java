784901524c81902c5b222c52058773adced81fbe
JuwonKim2022 <popolily12@gmail.com>
2023-01-08 01:12:52 +0900

    java - 20230108-2

---
+++ java/JavaCodingTest/src/main/java/Main7.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main7 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|" );
+        System.out.println();
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"'    |");
+        System.out.println("||_/=\\\\__|");
+        System.out.println();
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
