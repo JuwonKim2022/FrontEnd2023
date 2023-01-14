955a4f8da5d5485a9fc87b21e2a905a81dde89a3
JuwonKim2022 <popolily12@gmail.com>
2023-01-14 23:33:21 +0900

    py - 20230114

---
+++ java/JavaCodingTest/src/main/java/Main17.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main16 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        boolean val = (N>=0 && N<=99);
+
+        if(val) {
+            if(N<10){
+
+            }
+        }
+    }
+}
+
